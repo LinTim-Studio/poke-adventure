21243a033478e02fc17b4f48c058d293bf4e8946
TimothyEarley <michaelearley@192.168.2.105>
2014-09-05 21:16:47 +0200

    Projekterstellung

---
+++ src/pokeAdventure/Main.java
@@ -0,0 +1,17 @@
+package pokeAdventure;
+
+
+/**
+ * Die Main-Klasse des Spiels
+ */
+public class Main {
+
+	/**
+	 * Die mian-Methode des Spiels
+	 * @param args keine Auswirkung
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+	
+}
