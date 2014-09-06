fbdbcda86bd0cff3727c02787e4a3138d2cef0be
Linus Erhard <Linus Erhard@LinusErhard-PC>
2014-09-06 17:29:32 +0200

    Button2

--- src/pokeAdventure/state/menu/MenuButton.java
+++ src/pokeAdventure/state/menu/MenuButton.java
@@ -1,5 +1,43 @@
 package pokeAdventure.state.menu;
 
-public class MenuButton {
+import org.newdawn.slick.Image;
 
+import pokeAdventure.interfaces.Action;
+
+public class MenuButton 
+{
+	public float X ;
+	public float Y ;
+	public Image Bild;
+	
+	public Action event;
+	//public Image Bild2;
+	
+	
+	public MenuButton(float koorX, float koorY,Image pic)
+	{
+		X=koorX;
+		Y=koorY;
+		Bild=pic;
+	}
+	
+	public void zeichneButton()
+	{
+		Bild.draw(X,Y);
+	}
+	
+	public void buttonClick(float mx,float my)
+	{
+		if(this.clicked(mx,my))
+			this.event();
+			
+	}
+	
+	public boolean clicked(float mx, float my)
+	{
+		if((mx>X)&&(mx<X+(float) Bild.getHeight())&&(my>Y)&&(my<Y+(float) Bild.getWidth()))
+			return true;
+		else
+			return false;
+	}
 }
