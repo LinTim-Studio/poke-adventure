5c32c5f684749fa34b1b0ddf1adc88ecd51657c7
TimothyEarley <michaelearley@192.168.2.105>
2014-09-06 18:12:00 +0200

    Knöpfe im Menü

--- src/pokeAdventure/state/menu/Menu.java
+++ src/pokeAdventure/state/menu/Menu.java
@@ -3,20 +3,29 @@ package pokeAdventure.state.menu;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 import pokeAdventure.Main;
+import pokeAdventure.interfaces.Action;
 
 public class Menu extends BasicGameState {
+	
+	MenuButton newGame;
 
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException 
 	{
-		MenuButton newGame = new MenuButton();
-		MenuButton laden = new MenuButton();
-		MenuButton optionen= new MenuButton();
+		newGame = new MenuButton(100, 100, "res/menu/neuesSpiel.png", "res/menu/neuesSpielHighlight.png", new Action() {
+			@Override
+			public void action() {
+				System.out.println("Neues Spiel!");
+			}
+		});
+//		MenuButton laden = new MenuButton();
+//		MenuButton optionen= new MenuButton();
 	}
 
 	@Override
@@ -26,16 +35,20 @@ public class Menu extends BasicGameState {
 		g.fillRect(0, 0, container.getScreenWidth(), container.getScreenHeight());
 		
 		newGame.zeichneButton();
-		laden.zeichneButton();
-		optionen.zeichneButton();
+//		laden.zeichneButton();
+//		optionen.zeichneButton();
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException 
 	{
-		newGame.update();
-		laden.update();
-		optionen.update();
+		Input in = container.getInput();
+		int x = in.getMouseX();
+		int y = in.getMouseY();
+		boolean mouseDown = in.isMouseButtonDown(0);
+		newGame.update(x, y, mouseDown);
+//		laden.update();
+//		optionen.update();
 	}
 
 	@Override
