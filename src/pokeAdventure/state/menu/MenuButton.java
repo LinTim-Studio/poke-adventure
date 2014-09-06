4c6c4de24c432c74d0281769993f2463727b2b94
TimothyEarley <michaelearley@192.168.2.105>
2014-09-06 17:53:23 +0200

    Erweiterung des Knopfes

--- src/pokeAdventure/state/menu/MenuButton.java
+++ src/pokeAdventure/state/menu/MenuButton.java
@@ -1,41 +1,51 @@
 package pokeAdventure.state.menu;
 
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 
 import pokeAdventure.interfaces.Action;
 
-public class MenuButton 
-{
-	public int y ;
-	public int x ;
-	public Image Bild;
-	
+public class MenuButton {
+	public int y;
+	public int x;
+	public Image bild;
+	public Image bildHighlight;
+
 	public Action event;
-	//public Image Bild2;
-	
-	
-	public MenuButton(int koorX, int koorY,Image pic, Action aktion)
-	{
-		x=koorX;
-		y=koorY;
-		Bild=pic;
-		event=aktion;
+
+	private boolean ueber;
+
+	public MenuButton(int koorX, int koorY, String btnPath, String btnHighlightPath, Action aktion) throws SlickException {
+		this(koorX, koorY, new Image(btnPath), new Image(btnHighlightPath), aktion);
 	}
-	
-	public void zeichneButton()
-	{
-		Bild.draw(x,y);
+
+	public MenuButton(int koorX, int koorY, Image pic, Image picHighlight, Action aktion) {
+		x = koorX;
+		y = koorY;
+		bild = pic;
+		bildHighlight = picHighlight;
+		event = aktion;
 	}
-	
-	public void buttonClick(int mx,int my)
-	{
-		if(this.clicked(mx,my))
-			this.event.action();
+
+	public void zeichneButton() {
+		if (ueber)
+			bildHighlight.draw(x, y);
+		else
+			bild.draw(x, y);
 	}
-	
-	public boolean clicked(int mx, int my)
-	{
-		if((mx>x)&&(mx<x+ Bild.getHeight())&&(my>y)&&(my<y+ Bild.getWidth()))
+
+	public void update(int mx, int my, boolean mouseBtnDown) {
+		if (istUeber(mx, my)) {
+			ueber = true;
+			if (mouseBtnDown)
+				event.action();
+		} else
+			ueber = false;
+
+	}
+
+	public boolean istUeber(int mx, int my) {
+		if ((mx > x) && (mx < x + bild.getHeight()) && (my > y) && (my < y + bild.getWidth()))
 			return true;
 		else
 			return false;
