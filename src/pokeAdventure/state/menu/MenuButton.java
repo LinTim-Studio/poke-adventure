72d89453472058a0cb3def0b3a208ae954853b32
TimothyEarley <michaelearley@Michaels-iMac.local>
2014-09-08 22:47:16 +0200

    Optimierung der Knöpfe
    Anpassung an beliebige Größen des Displays;
    MenuButton kann sich jetzt automatisch anpassen

--- src/pokeAdventure/state/menu/MenuButton.java
+++ src/pokeAdventure/state/menu/MenuButton.java
@@ -14,6 +14,10 @@ public class MenuButton {
 	public Action event;
 
 	private boolean ueber;
+	/**
+	 * Bestimmt, ob der Knopf auf der x Achse zentriert sein soll.
+	 */
+	private boolean horizontalZentriert;
 
 	public MenuButton(int koorX, int koorY, String btnPath, String btnHighlightPath, Action aktion) throws SlickException {
 		this(koorX, koorY, new Image(btnPath), new Image(btnHighlightPath), aktion);
@@ -29,9 +33,9 @@ public class MenuButton {
 
 	public void zeichneButton() {
 		if (ueber)
-			bildHighlight.draw(x, y);
+			bildHighlight.draw(x + getHorizontaleVerschiebung(), y);
 		else
-			bild.draw(x, y);
+			bild.draw(x + getHorizontaleVerschiebung(), y);
 	}
 
 	public void update(int mx, int my, boolean mouseBtnDown) {
@@ -45,9 +49,26 @@ public class MenuButton {
 	}
 
 	public boolean istUeber(int mx, int my) {
-		if ((mx > x) && (mx < x + bild.getWidth()) && (my > y) && (my < y + bild.getHeight()))
+		if ((mx > x + getHorizontaleVerschiebung()) && (mx < x + getHorizontaleVerschiebung() + bild.getWidth()) && (my > y) && (my < y + bild.getHeight()))
 			return true;
 		else
 			return false;
 	}
+
+	public void setHorizontalZentriert(boolean horizontalZentriert) {
+		this.horizontalZentriert = horizontalZentriert;
+	}
+
+	/**
+	 * Ermittelt je nach Bildgröße die Verschiebung
+	 * @return die Verschiebung
+	 */
+	private int getHorizontaleVerschiebung() {
+		if (ueber)
+			return -((horizontalZentriert) ? bildHighlight.getWidth() / 2 : 0);
+		else
+			return -((horizontalZentriert) ? bild.getWidth() / 2 : 0);
+
+	}
+
 }
