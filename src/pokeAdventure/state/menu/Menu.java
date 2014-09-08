2c8bd3bf59522c965679e18c1285396413861873
TimothyEarley <michaelearley@Michaels-iMac.local>
2014-09-08 22:47:16 +0200

    Optimierung der Knöpfe
    Anpassung an beliebige Größen des Displays;
    MenuButton kann sich jetzt automatisch anpassen

--- src/pokeAdventure/state/menu/Menu.java
+++ src/pokeAdventure/state/menu/Menu.java
@@ -1,74 +1,81 @@
 package pokeAdventure.state.menu;
 
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
-import org.newdawn.slick.Image;
 
 import pokeAdventure.Main;
 import pokeAdventure.interfaces.Action;
 
 public class Menu extends BasicGameState {
-	
+
 	MenuButton newGame;
 	MenuButton laden;
 	MenuButton optionen;
+	
+	Image back;
 
 	@Override
-	public void init(GameContainer container, StateBasedGame game) throws SlickException 
-	{
-		newGame = new MenuButton(327, 150, "res/menu/neuesSpiel.png", "res/menu/neuesSpielHighlight.png", new Action() {
+	public void init(GameContainer container, StateBasedGame game) throws SlickException {
+		// Hintergrundbild laden
+		back = new Image("res/menu/menuHintergrund.png");
+		
+		// ein gemeinsames x für alle buttons
+		int x = container.getWidth() / 2;
+		// der Startwert für die y Richtung
+		int y = container.getHeight() / 3;
+		// Der zu addierende Wert pro button
+		int dy = (int) (container.getHeight() / 7.5); // 60
+		newGame = new MenuButton(x, y, "res/menu/neuesSpiel.png", "res/menu/neuesSpielHighlight.png", new Action() {
 			@Override
 			public void action() {
 				System.out.println("Neues Spiel!");
 			}
 		});
-		
-		laden = new MenuButton(327, 210, "res/menu/neuesSpiel.png", "res/menu/neuesSpielHighlight.png", new Action(){
+
+		laden = new MenuButton(x, y + dy, "res/menu/neuesSpiel.png", "res/menu/neuesSpielHighlight.png", new Action() {
 			@Override
-			public void action()
-			{
+			public void action() {
 				System.out.println("Laden!");
 			}
 		});
-		
-		optionen = new MenuButton(327, 270, "res/menu/neuesSpiel.png", "res/menu/neuesSpielHighlight.png", new Action(){
+
+		optionen = new MenuButton(x, y + 2 * dy, "res/menu/neuesSpiel.png", "res/menu/neuesSpielHighlight.png", new Action() {
 			@Override
-			public void action()
-			{
+			public void action() {
 				System.out.println("Optionen!");
 			}
 		});
+
+		newGame.setHorizontalZentriert(true);
+		laden.setHorizontalZentriert(true);
+		optionen.setHorizontalZentriert(true);
 	}
 
 	@Override
-	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException 
-	{
-		g.setColor(Color.green);
-		g.fillRect(0, 0, container.getScreenWidth(), container.getScreenHeight());
-		
-		Image back= new Image("res/menu/menuHintergrund.png");
-		back.draw(0,0);
-		
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+		// füllt automatisch den ganzen Bildschirm
+		back.draw(0, 0, container.getWidth(), container.getHeight());
+
 		newGame.zeichneButton();
 		laden.zeichneButton();
 		optionen.zeichneButton();
 	}
 
 	@Override
-	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException 
-	{
+	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
 		Input in = container.getInput();
 		int x = in.getMouseX();
 		int y = in.getMouseY();
 		boolean mouseDown = in.isMousePressed(0);
+		
 		newGame.update(x, y, mouseDown);
-		laden.update(x,y,mouseDown);
-		optionen.update(x,y,mouseDown);
+		laden.update(x, y, mouseDown);
+		optionen.update(x, y, mouseDown);
 	}
 
 	@Override
