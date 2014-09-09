6e2541d791e454728ffa650c64001e40ee43172e
Linus Erhard <Linus Erhard@LinusErhard-PC>
2014-09-09 18:55:00 +0200

    Andere Menus :D

---
+++ src/pokeAdventure/state/menu/Optionen.java
@@ -0,0 +1,39 @@
+package pokeAdventure.state.menu;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+import pokeAdventure.Main;
+import pokeAdventure.interfaces.Action;
+
+public class Optionen extends BasicGameState {
+
+	Image back;
+
+	@Override
+	public void init(GameContainer container, StateBasedGame game) throws SlickException {
+
+	}
+
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+
+	}
+
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+
+	}
+
+	@Override
+	public int getID() {
+		return Main.menuID;
+	}
+
+}
+
