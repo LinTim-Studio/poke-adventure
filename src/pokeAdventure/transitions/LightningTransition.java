34d93922b4e52d58dcf6a3461e585ec337b3fbc8
TimothyEarley <michaelearley@Michaels-iMac.local>
2014-10-13 17:11:21 +0200

    Weiter Zentralisierung der Bilder (Lightning)

--- src/pokeAdventure/transitions/LightningTransition.java
+++ src/pokeAdventure/transitions/LightningTransition.java
@@ -4,27 +4,20 @@ import java.util.Random;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.GameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.Transition;
 
-import pokeAdventure.util.error.Fehlermelder;
+import pokeAdventure.util.SpriteManager;
 
 public class LightningTransition implements Transition {
-	private static int frameCount = 3;
-
-	private static Image[] frames;
-	private static boolean initialized;
-
 	private int time, x;
 	private boolean done;
 	private Random rand;
 
 	public LightningTransition(int time) {
 		this.time = time;
-		staticInit();
 	}
 
 	@Override
@@ -44,8 +37,8 @@ public class LightningTransition implements Transition {
 
 	@Override
 	public void postRender(StateBasedGame game, GameContainer container, Graphics g) throws SlickException {
-		for (int i = 0; i < frameCount; i++) {
-			frames[i].draw(x, 0, container.getWidth(), container.getHeight());
+		for (int i = 0; i < SpriteManager.lightning.length; i++) {
+			SpriteManager.lightning[i].draw(x, 0, container.getWidth(), container.getHeight());
 		}
 
 	}
@@ -53,22 +46,6 @@ public class LightningTransition implements Transition {
 	@Override
 	public boolean isComplete() {
 		return done;
-	}
-
-	private static boolean staticInit() {
-		if (initialized) {
-			return false;
-		}
-		frames = new Image[frameCount];
-		for (int i = 1; i <= frameCount; i++) {
-			try {
-				frames[i - 1] = new Image("lightning/lightning" + i + ".png");
-			} catch (SlickException e) {
-				Fehlermelder.melde(e);
-			}
-		}
-		initialized = true;
-		return true;
 	}
 
 	@Override
--- src/pokeAdventure/util/SpriteManager.java
+++ src/pokeAdventure/util/SpriteManager.java
@@ -1,5 +1,9 @@
 package pokeAdventure.util;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
@@ -14,49 +18,76 @@ public abstract class SpriteManager {
 	public static Image geschlechtJunge, geschlechtJungeOver, geschlechtMaedchen, geschlechtMaedchenOver, geschlechtAnders, geschlechtAndersOver;
 	public static Image pikachu, papierbg, prof;
 	public static Image sliderBack, slider, sliderOver;
-	
+	public static Image[] lightning;
+
 	public static void load() {
 		try {
 
-		// menu
+			// menu
 
-		menuBack = new Image("res/menu/menu.png");
-		
-		btnNeuesSpiel = new Image("res/menu/buttons/neuesSpiel.png");
-		btnNeuesSpielOver = new Image("res/menu/buttons/neuesSpielOver.png");
+			menuBack = new Image("res/menu/menu.png");
 
-		btnSpielLaden = new Image("res/menu/buttons/laden.png");
-		btnSpielLadenOver = new Image("res/menu/buttons/ladenOver.png");
+			btnNeuesSpiel = new Image("res/menu/buttons/neuesSpiel.png");
+			btnNeuesSpielOver = new Image("res/menu/buttons/neuesSpielOver.png");
 
-		btnOptionen = new Image("res/menu/buttons/optionen.png");
-		btnOptionenOver = new Image("res/menu/buttons/optionenOver.png");
+			btnSpielLaden = new Image("res/menu/buttons/laden.png");
+			btnSpielLadenOver = new Image("res/menu/buttons/ladenOver.png");
 
-		btnBeenden = new Image("res/menu/buttons/beenden.png");
-		btnBeendenOver = new Image("res/menu/buttons/beendenOver.png");
+			btnOptionen = new Image("res/menu/buttons/optionen.png");
+			btnOptionenOver = new Image("res/menu/buttons/optionenOver.png");
 
-		// start
-		geschlechtJunge = new Image("res/gameStart/jungeIcon.png");
-		geschlechtJungeOver = new Image("res/gameStart/jungeIconOver.png");
+			btnBeenden = new Image("res/menu/buttons/beenden.png");
+			btnBeendenOver = new Image("res/menu/buttons/beendenOver.png");
 
-		geschlechtAnders = new Image("res/gameStart/anderesIcon.png");
-		geschlechtAndersOver = new Image("res/gameStart/anderesIconOver.png");
+			// start
+			geschlechtJunge = new Image("res/gameStart/jungeIcon.png");
+			geschlechtJungeOver = new Image("res/gameStart/jungeIconOver.png");
 
-		geschlechtMaedchen = new Image("res/gameStart/maedchenIcon.png");
-		geschlechtMaedchenOver = new Image("res/gameStart/maedchenIconOver.png");
-		
-		//slider
-		sliderBack = new Image("res/menu/slider/sliderBack.png");
-		slider = new Image("res/menu/slider/slider.png");
-		sliderOver = new Image("res/menu/slider/sliderOver.png");
-		
-		//misc
-		pikachu = new Image("res/gameStart/pikachu.png");
-		papierbg = new Image("res/gameStart/Papierbg.png");
-		prof = new Image("res/gameStart/prof.png");
-		
+			geschlechtAnders = new Image("res/gameStart/anderesIcon.png");
+			geschlechtAndersOver = new Image("res/gameStart/anderesIconOver.png");
+
+			geschlechtMaedchen = new Image("res/gameStart/maedchenIcon.png");
+			geschlechtMaedchenOver = new Image("res/gameStart/maedchenIconOver.png");
+
+			// slider
+			sliderBack = new Image("res/menu/slider/sliderBack.png");
+			slider = new Image("res/menu/slider/slider.png");
+			sliderOver = new Image("res/menu/slider/sliderOver.png");
+
+			// misc
+			pikachu = new Image("res/gameStart/pikachu.png");
+			papierbg = new Image("res/gameStart/Papierbg.png");
+			prof = new Image("res/gameStart/prof.png");
+
+			// lightning
+			lightning = loadAllImagesFromDir("res/lightning/");
+
 		} catch (SlickException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static Image[] loadAllImagesFromDir(String dirPath) {
+		ArrayList<Image> lighningArray = new ArrayList<Image>();
+
+		File dir = new File(dirPath);
+		if (!dir.isDirectory())
+			return null;
+
+		for (File file : dir.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return (name.endsWith(".png"));
+			}
+		})) {
+			try {
+				lighningArray.add(new Image(file.getPath()));
+			} catch (SlickException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return lighningArray.toArray(new Image[] {});
 	}
 
 }
